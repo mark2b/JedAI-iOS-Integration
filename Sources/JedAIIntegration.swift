465391962393f7ce3b605f31b28027960792ad9a
Mark Berner <mark2b@gmail.com>
2022-02-27 10:08:46 +0200

    no message

---
+++ Sources/JedAIIntegration.swift
@@ -0,0 +1,8 @@
+//
+//  JedAIIntegration.swift
+//  JedAIIntegrationKit
+//
+//  Created by Mark Berner on 27/02/2022.
+//
+
+import Foundation
---
+++ Sources/jedai_integration.h
@@ -0,0 +1,18 @@
+//
+//  jedai_integration.h
+//  jedai-integration
+//
+//  Created by Mark Berner on 26/02/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for jedai_integration.
+FOUNDATION_EXPORT double jedai_integrationVersionNumber;
+
+//! Project version string for jedai_integration.
+FOUNDATION_EXPORT const unsigned char jedai_integrationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <jedai_integration/PublicHeader.h>
+
+
